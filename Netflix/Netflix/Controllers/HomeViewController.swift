137cb0d00dd84f272ecff8661869ad3a8cd54cee
JonettaPek <jpek009@e.ntu.edu.sg>
2023-12-12 00:17:20 +0800

    set up nav bar

--- Netflix/Netflix/Controllers/HomeViewController.swift
+++ Netflix/Netflix/Controllers/HomeViewController.swift
@@ -15,6 +15,19 @@ class HomeViewController: UIViewController {
         return table
     }()
     
+    private func configureNavbar() {
+        var image = UIImage(named: "netflix-logo")
+        image = image?.withRenderingMode(.alwaysOriginal)
+        navigationItem.leftBarButtonItem = UIBarButtonItem(image: image, style: .done, target: self, action: nil)
+        
+        navigationItem.rightBarButtonItems = [
+            UIBarButtonItem(image: UIImage(systemName: "person"), style: .done, target: self, action: nil),
+            UIBarButtonItem(image: UIImage(systemName: "play.rectangle"), style: .done, target: self, action: nil)
+        ]
+        
+        navigationController?.navigationBar.tintColor = .white
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         view.backgroundColor = .systemBackground
@@ -24,6 +37,8 @@ class HomeViewController: UIViewController {
         homeFeedTable.dataSource = self
         
         homeFeedTable.tableHeaderView = HeroHeaderUIView(frame: CGRect(x: 0, y: 0, width: view.bounds.width, height: 450))
+        
+        configureNavbar()
     }
     
     override func viewDidLayoutSubviews() {
@@ -50,7 +65,15 @@ extension HomeViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
         return 200
     }
+    
     func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
         return 40
+    }
+    
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        let defaultOffset = view.safeAreaInsets.top
+        let offset = scrollView.contentOffset.y + defaultOffset
+        
+        navigationController?.navigationBar.transform = .init(translationX: 0, y: min(0, -offset))
     }
 }
