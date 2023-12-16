be964e4d66f5cce78eca78ffcddc57fb52436cb6
JonettaPek <jpek009@e.ntu.edu.sg>
2023-12-16 14:58:32 +0800

    set up table view for upcoming view controller

--- Netflix/Netflix/Controllers/UpcomingViewController.swift
+++ Netflix/Netflix/Controllers/UpcomingViewController.swift
@@ -8,9 +8,60 @@
 import UIKit
 
 class UpcomingViewController: UIViewController {
+    
+    private var titles: [Title] = [Title]()
 
+    private let upcomingTable: UITableView = {
+        let table = UITableView()
+        table.register(UITableViewCell.self, forCellReuseIdentifier: "cell")
+        return table
+    }()
+    
+    private func fetchUpcoming() {
+        APICaller.shared.getUpcomingMovies { [weak self] result in
+            switch result {
+            case .success(let titles):
+                self?.titles = titles
+                DispatchQueue.main.async {
+                    self?.upcomingTable.reloadData()
+                }
+            case .failure(let error):
+                print(error)
+            }
+        }
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        title = "upcoming"
         view.backgroundColor = .systemBackground
+        
+        navigationController?.navigationBar.prefersLargeTitles = true
+        navigationController?.navigationItem.largeTitleDisplayMode = .always
+        
+        view.addSubview(upcomingTable)
+        upcomingTable.delegate = self
+        upcomingTable.dataSource = self
+        
+        fetchUpcoming()
     }
+    
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        upcomingTable.frame = view.bounds
+    }
+}
+
+extension UpcomingViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return titles.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as? UITableViewCell else { return UITableViewCell() }
+        cell.textLabel?.text = titles[indexPath.row].original_name ?? titles[indexPath.row].original_title ?? "Unknown"
+        return cell
+    }
+    
+    
 }
